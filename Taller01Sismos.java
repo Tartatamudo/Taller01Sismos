caa2faecdd0c43e2af85c5b4bf8d866bf3511ef4
Tartatamudo <114184223+Tartatamudo@users.noreply.github.com>
2023-03-23 10:12:21 -0300

    Update Taller01Sismos.java
    En este commit relleno todos los metodos.
    agrege 2 metodos que estan en funcionamiento y son:
    1. para imprimir el resultado del metodo:  SismosMayoresA5, que fue imprimirNumSismosMayores5
    2.para imprimir el resultado del metodo:  BuscarSismosMayor, que fue ImprimirSismoMayor
    Ahora me falta comprobar el funcionamiento del metodo menu

--- Taller01Sismos.java
+++ Taller01Sismos.java
@@ -2,29 +2,28 @@ import java.util.Random;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        double a[][] = new double[24][7];
+        a = IngresarDatos();
+        SismosSmsMayorOIguala7(a);
     }
 
-    public static double[][] IngresarDatos(){
+    public static double[][] IngresarDatos() {
         double Sismos[][] = new double[24][7];
-        for (int i = 0; i < Sismos.length ; i++) {
+        for (int i = 0; i < Sismos.length; i++) {
             for (int j = 0; j < Sismos[i].length; j++) {
-                Sismos[i][j] = (Math.random()*9.9);
+                Sismos[i][j] = (Math.random() * 9.9);
             }
         }
         return Sismos;
     }
 
-    public static void menu(){
-
-    }
-
-    public static double BuscarSismoMayor(double Sismos[][]){
+    public static double BuscarSismoMayor(double Sismos[][]) {
         double max = 0.0;
         int posI = 0;
         int posJ = 0;
-        for (int i = 0; i < Sismos.length ; i++) {
+        for (int i = 0; i < Sismos.length; i++) {
             for (int j = 0; j < Sismos[i].length; j++) {
-                if (Sismos[i][j] > max){
+                if (Sismos[i][j] > max) {
                     max = Sismos[i][j];
                     posI = i;
                     posJ = j;
@@ -32,6 +31,11 @@ public class Main {
             }
         }
         return max;
+    }
+
+    public static void ImprimirSismoMAyor(double Sismos[][]) {
+        double max = BuscarSismoMayor(Sismos);
+        System.out.printf("El sismo mas grande es de: " + max);
     }
 
     public static int SismosMayoresA5(double Sismos[][]) {
@@ -46,7 +50,54 @@ public class Main {
         return acum;
     }
 
-    public static void SismosSmsMayorOIguala7(){
+    public static void imprimirNumSismosMayores5(double Sismos[][]) {
+        int num = SismosMayoresA5(Sismos);
+        System.out.printf("EL numero de sismos mayores o iguales a 5.0 son: " + num);
     }
 
+    public static void SismosSmsMayorOIguala7(double Sismos[][]) {
+        int acum = 0;
+        for (int i = 0; i < Sismos.length; i++) {
+            for (int j = 0; j < Sismos[i].length; j++) {
+                if (Sismos[i][j] >= 7.0) {
+                    acum = acum + 1;
+                    System.out.println("Alerta!!! se debe evacuar zona costera!, Sismo de: " + Sismos[i][j]);
+                }
+            }
+        }
+    }
+
+    public static void menu() {
+        Scanner teclado = new Scanner(System.in);
+        double Sismos[][] = new double[24][7];
+        int eleccion = 0;
+        while ((0 < eleccion) && (eleccion < 5)) {
+            System.out.println("Eliga la opcion que quiere hacer:");
+            System.out.println("[1]Ingresar datos");
+            System.out.println("[2]Mostrar sismo de mayor magnitud");
+            System.out.println("[3]Contar sismos mayores o iguales a 5.0");
+            System.out.println("[4]Enviar SMS por cada sismo mayor o igual a 7.0");
+            System.out.println("Si desea salir debe ingresar cualquier otro numero");
+            eleccion = teclado.nextInt();
+            switch (eleccion) {
+                case 1:
+                    Sismos = IngresarDatos();
+                    break;
+                case 2:
+                    ImprimirSismoMAyor(Sismos);
+                    break;
+                case 3:
+                    imprimirNumSismosMayores5(Sismos);
+                    break;
+                case 4:
+                    SismosSmsMayorOIguala7(Sismos);
+                    break;
+                default:
+                    System.out.println("Salio del programa exitosamente");
+                    break;
+            }
+
+
+        }
+    }
 }
