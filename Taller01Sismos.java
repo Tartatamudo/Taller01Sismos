a6f20aff2e16b24d134cc4dfbd32bb740a6172e0
Tartatamudo <114184223+Tartatamudo@users.noreply.github.com>
2023-03-23 09:11:24 -0300

    Create Taller01Sismos.java

---
+++ Taller01Sismos.java
@@ -0,0 +1,23 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public static double IngresarDatos(){
+        return Sismos;
+    }
+
+    public static void menu(){
+    }
+
+    public static void BuscarSismoMayor(){
+    }
+
+    public static void SismosMayoresA5(){
+    }
+
+
+}
